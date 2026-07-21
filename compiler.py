b1f42d6f5f675c221d5528739a274d7804e73d5b
MACsepehri <macsepehri@gmail.com>
2026-07-21 09:36:24 +0330

    Removed calling undefined functions in compile() function.

--- compiler.py
+++ compiler.py
@@ -214,8 +214,6 @@ class Compiler:
         self.read_var()
         self.read_defined_func()
         self.read_var_value_as_function()
-        self.create_function()
-        self.read_user_created_function()
         self.print_value()
         print(self.func)
 
